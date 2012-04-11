6be716a3430a33790ef0719c4f38f78dd7d16cdd
Xananax <xananax@yelostudio.com>
2012-04-11 23:35:48 +0300

    first commit

---
+++ _ss_environment.php
@@ -0,0 +1,7 @@
+<?php
+global $_FILE_TO_URL_MAPPING;
+$_FILE_TO_URL_MAPPING['/home/xananax/www'] = 'http://localhost';
+$_FILE_TO_URL_MAPPING['/home/xananax/www/jemarch'] = 'http://localhost/jemarch';
+$_FILE_TO_URL_MAPPING['/media/things/home/xannax/Projects/php'] = 'http://localhost';
+$_FILE_TO_URL_MAPPING['/media/things/home/xannax/Projects/php/jemarch'] = 'http://localhost/jemarch';
+
